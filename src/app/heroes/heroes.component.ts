85e1be485ed2fdab8227cad9c038a2f4630a5291
krina <krinapatel4793@gmail.com>
2019-11-10 21:25:51 +1300

    services added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
 export class HeroesComponent implements OnInit {
 
-  heroes: Hero[];
   selectedHero: Hero;
+
+  heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
 
@@ -23,7 +23,7 @@ export class HeroesComponent implements OnInit {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  
+
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
